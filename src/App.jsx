b0928e1ece0451b07186d3487ffa8ce79d7e10da
Pedro Ecsedei <pedro.martins55757@gmail.com>
2024-04-04 20:27:25 -0300

    style: about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header/Header";
 import Banner from "./components/Home/Banner";
 import './scss/style.scss';
 
-
 export default function App() {
   return (
     <div id="app">
--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -3,15 +3,14 @@ import image from '../../assets/about.png'
 
 export default function About() {
     return (
-        <section className='container'>
-            <div className='text-content'>
+        <section className='about'>
+            <div className='text-content half-container'>
                 <h2>Sobre</h2>
                 <p>Potter ipsum wand elf parchment wingardium. Headmaster dervish restricted toad-like headmaster find granger clean hat biting. Butter easy cloak hall treacle scales ground. Section bathroom cadogan trelawney silver kidney black. Hungarian back doe scabbers frisbees shunpike potter chess. Devil’s witch thestral voldemort drills hand. You mcgonagall floor them daily them honeydukes.</p>
                 <p>Saiba Mais <ArrowRightIcon/></p>
             </div>
-            <div>
-                <img src={image} alt="woman workout" className='image-about'/>
-            </div>
+
+            <div className='half-container image-about' />
         </section>
     )
 }
