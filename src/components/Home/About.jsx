3a8eb96437cddfd9fa62d0894f1a894c86a7245a
Thiago <thiago_99_oliveira@hotmail.com>
2024-04-04 20:03:07 -0300

    alteração no about mudando as colunas para container

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,21 +1,17 @@
 import { ArrowRightIcon } from 'lucide-react';
 import image from '../../assets/about.png'
 
-import { Col, Row } from "react-bootstrap";
-
 export default function About() {
     return (
-        <div className='container-2'>
-            <Row className='about-section'>
-                <Col lg={8} className='text-content'>
-                    <h2>Sobre</h2>
-                    <p>Potter ipsum wand elf parchment wingardium. Headmaster dervish restricted toad-like headmaster find granger clean hat biting. Butter easy cloak hall treacle scales ground. Section bathroom cadogan trelawney silver kidney black. Hungarian back doe scabbers frisbees shunpike potter chess. Devil’s witch thestral voldemort drills hand. You mcgonagall floor them daily them honeydukes.</p>
-                    <p>Saiba Mais <ArrowRightIcon/></p>
-                </Col>
-                <Col md={4}>
-                    <img src={image} alt="woman workout"/>
-                </Col>
-            </Row>
-        </div>
+        <section className='container'>
+            <div className='text-content'>
+                <h2>Sobre</h2>
+                <p>Potter ipsum wand elf parchment wingardium. Headmaster dervish restricted toad-like headmaster find granger clean hat biting. Butter easy cloak hall treacle scales ground. Section bathroom cadogan trelawney silver kidney black. Hungarian back doe scabbers frisbees shunpike potter chess. Devil’s witch thestral voldemort drills hand. You mcgonagall floor them daily them honeydukes.</p>
+                <p>Saiba Mais <ArrowRightIcon/></p>
+            </div>
+            <div>
+                <img src={image} alt="woman workout" className='image-about'/>
+            </div>
+        </section>
     )
 }
