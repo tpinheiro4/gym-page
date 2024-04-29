5fa43ae8b0d2ec4d34b66f963f41aec2cec9df6c
Pedro Ecsedei <pedro.martins55757@gmail.com>
2024-04-29 17:26:57 -0300

    build: configuring strapi backend

--- backend/gym-strapi/types/generated/contentTypes.d.ts
+++ backend/gym-strapi/types/generated/contentTypes.d.ts
@@ -695,7 +695,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     username: Attribute.String &
@@ -724,6 +723,7 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.role'
     >;
+    profile_slug: Attribute.UID<'plugin::users-permissions.user', 'username'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
